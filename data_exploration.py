e821ce4215260818bb4947a8c8e27f0ac3d47dc0
Maria Pantsiou <maria.th.pantsiou@gmail.com>
2021-04-06 00:23:14 +0100

    Add vizs

--- data_exploration.py
+++ data_exploration.py
@@ -1,6 +1,5 @@
 import pandas as pd
-import numpy as np
-import seaborn as sns
+from utils import subplot_correlation_matrix, plot_heatmap
 
 # TODO add heatmap: seaborn.heatmap(data, *, vmin=None, vmax=None, cmap=None,
 # center=None, robust=False, annot=None, fmt='.2g', annot_kws=None,
@@ -9,9 +8,29 @@ import seaborn as sns
 # **kwargs)
 
 # exploration
-a = pd.read_csv('data/training.csv')
-a.set_index('id', inplace=True)
-a['signal'].values_counts()
-a.corr()
-a.columns[(a.corr()['LifeTime']>0.5).values]
+train_df = pd.read_csv('data/training.csv')
+train_df.set_index('id', inplace=True)
+
+# check couts of values
+train_df['signal'].value_counts()
+
+# check correlations
+train_df.corr()
+train_df.columns[(train_df.corr()['LifeTime'] > 0.5).values]
+
+# nan values
+len(train_df) - len(train_df.dropna())
+
+# check data types of columns
+train_df.dtypes
+
+# describe
+train_df.describe()
+
+
+subplot_correlation_matrix(train_df, (30, 30))
+
+plot_heatmap(df=train_df, columns=train_df.columns, figsize=(10, 8), annot_fontsize=6)
+
+test_df = pd.read_csv('data/test.csv')
 
