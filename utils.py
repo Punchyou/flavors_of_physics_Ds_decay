5085fe597674c5a732a4321f1f4fa0cf0a8b5c38
Maria Pantsiou <maria.th.pantsiou@gmail.com>
2021-04-08 23:20:12 +0100

    Add create transformed df

--- utils.py
+++ utils.py
@@ -66,5 +66,67 @@ def plot_heatmap(df, columns, vmin=-1, vmax=1, cmap="RdBu", annot=True,
     plt.yticks(size=yticks_fontsize)
 
 
+# TODO check doscstring + add params
+def display_component(pca_fitted_model, num_of_components, features_list,
+                      component_number, n_weights_to_display=10):
+    """
+    Examine the makeup of each PCA component based on the weightings
+    of the original features that are included in the component.
+    Note that the components are ordered from smallest to largest
+    and so we are getting the correct rows by calling
+    num_of_components-component_number to get the component_number
+    component.
+    """
+    # get index of component (last row - component_num)
+    row_idx = num_of_components - component_number
+
+    components_makeup = pd.DataFrame(pca_fitted_model.components_)
+    # get the list of weights from a row in v, dataframe
+    v_1_row = components_makeup.iloc[:, row_idx]
+    v_1 = np.squeeze(v_1_row.values)
+
+    # match weights to features in counties_scaled dataframe
+    comps = pd.DataFrame(list(zip(v_1, features_list)), 
+                         columns=['weights', 'features'])
+
+    # we'll want to sort by the largest n_weights
+    # weights can be neg/pos and we'll sort by magnitude
+    comps['abs_weights'] = comps['weights'].apply(lambda x: np.abs(x))
+    sorted_weight_data = comps.sort_values('abs_weights',
+                                           ascending=False).head(n_weights_to_display)
+
+    # display using seaborn
+    ax = plt.subplots(figsize=(10, 6))
+    ax = sns.barplot(data=sorted_weight_data,
+                     x="weights",
+                     y="features",
+                     palette="Blues_d")
+    ax.set_title("PCA Component Makeup, Component #" + str(component_number))
+    plt.show()
+
+
+# TODO add docstring
+def create_transformed_df(pca_components, scaled_train_df, num_of_components, n_top_components):
+    # create new dataframe to add data to
+    df = pd.DataFrame()
+
+    # for each of our new, transformed data points
+    # append the component values to the dataframe
+    for data in pca_components:
+        # get component values for each data point
+        components = data.label['projection'].float32_tensor.values
+        df = df.append([list(components)])
+
+    # index by county, just like counties_scaled
+    df.index = scaled_train_df.index
+
+    # keep only the top n components
+    start_idx = num_of_components - n_top_components
+    df = df.iloc[:, start_idx:]
+
+    # reverse columns, component order
+    return df.iloc[:, ::-1]i
+
+
 
 
