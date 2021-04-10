ae9fd48eb15b858376ff734ef0762f539c46c610
Maria Pantsiou <maria.th.pantsiou@gmail.com>
2021-04-10 20:47:37 +0100

    WIP: Add knn block

--- data_exploration.py
+++ data_exploration.py
@@ -1,16 +1,15 @@
 import pandas as pd
-from utils import (
-    subplot_correlation_matrix,
-    plot_heatmap,
-    display_components,
-    create_transform
-)
+from utils import subplot_correlation_matrix, plot_heatmap, display_component, create_transformed_df
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 import seaborn as sns
+from sklearn import preprocessing
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
+
 
 # exploration
 # load data
@@ -108,6 +107,40 @@ create_transformed_df
 # TODO try dimnetionality_reduction_knn.py function for pca dn knn
 
 
+# find optimal value based on error
+
+from_, to = 40, 80
+error_rate = pd.DataFrame(index=range(from_, to), columns=['rate'])
+for i in range(from_, to):
+    knn = KNeighborsClassifier(n_neighbors=i)
+    knn.fit(X_train, y_train)
+    pred_i = knn.predict(X_test)
+    error_rate.loc[] = error_rate.append(np.mean(pred_i != y_test))
+
+plt.figure(figsize=(10, 6))
+plt.plot(range(from_, to), error_rate.values, color='blue', linestyle='dashed', marker='o', markerfacecolor='red', markersize=10)
+plt.title('Error Rate vs. K Value')
+plt.xlabel('K')
+plt.ylabel('Error Rate')
+print("Minimum error:-", min(error_rate), "at K =",
+      error_rate[error_rate['rate']==min(error_rate)].index)
+plt.show()
+
+# find optimal k values based on accuracy
+acc = []
+# Will take some time
+for i in range(1,40):
+    neigh = KNeighborsClassifier(n_neighbors = i).fit(X_train,y_train)
+    yhat = neigh.predict(X_test)
+    acc.append(metrics.accuracy_score(y_test, yhat))
+
+plt.figure(figsize=(10,6))
+plt.plot(range(40,100),acc,color = 'blue',linestyle='dashed', marker='o', markerfacecolor='red', markersize=10)
+plt.title('accuracy vs. K Value')
+plt.xlabel('K')
+plt.ylabel('Accuracy')
+print("Maximum accuracy:-", max(acc), "at K =", acc.index(max(acc)))
+
 
 
 
@@ -123,5 +156,9 @@ This curve quantifies how much of the total, 64-dimensional variance is containe
 
 Sources:
 https://jakevdp.github.io/PythonDataScienceHandbook/05.09-principal-component-analysis.html
+https://towardsdatascience.com/how-to-find-the-optimal-value-of-k-in-knn-35d936e554eb
+Data Science from Scratch
+Machine learning with python
+Feature Egnineering
 
 """
