de192bc4bf9a94a8d84bcca12817187de0e0a170
Maria Pantsiou <maria.th.pantsiou@gmail.com>
2021-04-10 21:18:40 +0100

    Add line

--- dimentionality_reduction_knn.py
+++ dimentionality_reduction_knn.py
@@ -1,16 +1,12 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.neighbors import (KNeighborsClassifier,
-                               NeighborhoodComponentsAnalysis)
+from sklearn.neighbors import (KNeighborsClassifier, NeighborhoodComponentsAnalysis)
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
-
-print(__doc__)
 
 n_neighbors = 3
 random_state = 0
