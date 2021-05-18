0fd3d9d15de606b834dbf0ce062cb8811958c249
Maria Pantsiou <maria.th.pantsiou@gmail.com>
2021-05-18 23:22:59 +0100

    Add second xgboost optimization

--- models_exploration.py
+++ models_exploration.py
@@ -21,36 +21,24 @@ from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import MinMaxScaler, RobustScaler, StandardScaler
 from xgboost import XGBClassifier, cv as xgb_cv, DMatrix as xgb_dmatrix, train as xgb_train
 from bayes_opt import BayesianOptimization
-from utils import *
+from utils import gather_performance_metrics, range_inc
+from skopt import BayesSearchCV
+from sklearn.model_selection import StratifiedKFold
 
 
 
 
 def main():
-    # TODO: remote * imports
-    # TODO add pipeline for models that take in scaled data
-    # TODO add a func to fit and return predictions
-    # TODO add a baseline model
     # load data
     # train_df = pd.read_csv('data/training.csv', index_col='id')
-    train_df = pd.read_csv("check_agreement.csv", index_col="id")
+    train_df = pd.read_csv("data/resampled_data.csv", index_col="Unnamed: 0")
     y = train_df["signal"]
-    # according to the dataset description, weights are used to
-    # determine if a decay is signal or background event
-    X = train_df.drop(["weight", "signal"], axis=1)
-
-    # TODO move it to the data exporation file
-    # check distributions
-    # X.hist(figsize=(60, 20))
-    # plt.show()
-
-    # undersample to create balanced dataset
-    undersampler = RandomUnderSampler(random_state=42)
-    X_resampled, y_resampled = undersampler.fit_resample(X, y)
+    X = train_df.drop("signal", axis=1)
 
     # split in X and y
     X_train, X_test, y_train, y_test = train_test_split(
-        X_resampled, y_resampled, test_size=0.2
+        X, y, test_size=0.2,
+        random_state=42
     )
 
     # TODO move to the data exploration file
@@ -69,138 +57,187 @@ def main():
     X_train_rob_scaled = rob_scaler.fit_transform(X_train)
     X_test_rob_scaled = rob_scaler.fit_transform(X_test)
 
-    # baseline model
-    knn_bsl = KNeighborsClassifier()
-    knn_bsl.fit(X_train_rob_scaled, y_train)
-    knn_bsl_prediction = knn_bsl.predict(X_test_rob_scaled)
-    metrics_df = gather_performance_metrics(
-        y_true=y_test, y_pred=knn_bsl_prediction, model_col="knn_baseline"
-    )
+    metrics_df = pd.DataFrame()
+    for X_train, X_test, scale in [(X_train_minmax_scaled, X_test_minmax_scaled, "minmax"), (X_train_std_scaled, X_test_std_scaled, "std"), (X_train_rob_scaled, X_test_rob_scaled, "rob")]:
 
-    # # model 1 - KNN
-    # knn_clf = KNeighborsClassifier()
-    # # make use of grid search as is relatively fast for knn
-    # knn_rs_cv = GridSearchCV(estimator=knn_clf, param_grid={'n_neighbors': range(1, 60)})
-    # knn_rs_cv.fit(X=X_train_rob_scaled, y=y_train)
-    # knn_prediction = knn_rs_cv.predict(X_test_rob_scaled)
-    # metrics_df = metrics_df.append(gather_performance_metrics(y_true=y_test,
-    #                                                           y_pred=knn_prediction,
-    #                                                           model_col="knn"))
-    # # plotting prec/recall curve
-    # # plot_precision_recall_curve(estimator=knn_clf, X=X_train, y=y_train)
-    # # plt.show()
+    
+        # # model 1 - KNN
+        knn_clf = KNeighborsClassifier()
+        # make use of grid search as is relatively fast for knn
+        knn_rs_cv = GridSearchCV(estimator=knn_clf, param_grid={'n_neighbors': range(1, 60)})
+        knn_rs_cv.fit(X=X_train, y=y_train)
+        knn_prediction = knn_rs_cv.predict(X_test)
+        metrics_df = metrics_df.append(gather_performance_metrics(y_true=y_test,
+                                                                  y_pred=knn_prediction,
+                                                                  model_col=f"knn_{scale}"))
+        # plotting prec/recall curve
+        # plot_precision_recall_curve(estimator=knn_clf, X=X_train, y=y_train)
+        # plt.show()
+    
+        # model 2 - SDG: Stohastic Gradient Descient (linear SVM)
+        # minimizes the loss function and the regularization term (penalty, for overfitting)
+        sgd_clf = SGDClassifier()
+        sgd_rs_cv = RandomizedSearchCV(
+            estimator=sgd_clf,
+            param_distributions={
+                "loss": [
+                    "log",
+                    "hinge",
+                    "modified_huber",
+                    "squared_hinge",
+                    "perceptron",
+                    "squared_loss",
+                    "huber",
+                    "epsilon_insensitive",
+                    "squared_epsilon_insensitive",
+                ],
+                "penalty": ["l2", "l1"],
+                "alpha": list(range_inc(0.001, 50, 0.01, 1.5, 3)),
+                "early_stopping": [True],
+                "random_state": [42],
+            },
+        )
+        sgd_rs_cv.fit(X_train, y_train)
+        sgd_prediction = sgd_rs_cv.predict(X_test)
+        metrics_df = metrics_df.append(gather_performance_metrics(y_test,
+                                                                  sgd_prediction,
+                                                                  f"sgd_{scale}"))
+    
+        # model 3 - support vector machines
+        svm_clf = svm.SVC()
+        svm_clf.fit(X_train, y_train)
+        svm_prediction = svm_clf.predict(X_test)
+    
+        svm_rs_cv = RandomizedSearchCV(
+            estimator=svm_clf,
+            param_distributions={
+                "C": list(range_inc(0.5, 100, 0.9, 1.2)),
+                "break_ties": [False],
+                "decision_function_shape": ["ovo", "ovr"],
+                "kernel": ["poly", "rbf", "sigmoid"],
+                "random_state": [42] 
+            }
+        )
+        svm_rs_cv.fit(X_train, y_train)
+        svm_prediction = svm_rs_cv.predict(X_test)
+        metrics_df = metrics_df.append(gather_performance_metrics(y_true=y_test,
+                                                                  y_pred=svm_prediction,
+                                                                  model_col=f"svm_{scale}"))
 
-    # # model 2 - SDG: Stohastic Gradient Descient (linear SVM)
-    # # minimizes the loss function and the regularization term (penalty, for overfitting)
-    # sgd_clf = SGDClassifier()
-    # sgd_rs_cv = RandomizedSearchCV(
-    #     estimator=sgd_clf,
-    #     param_distributions={
-    #         "loss": [
-    #             "log",
-    #             "hinge",
-    #             "modified_huber",
-    #             "squared_hinge",
-    #             "perceptron",
-    #             "squared_loss",
-    #             "huber",
-    #             "epsilon_insensitive",
-    #             "squared_epsilon_insensitive",
-    #         ],
-    #         "penalty": ["l2", "l1"],
-    #         "alpha": list(range_inc(0.001, 50, 0.01, 1.5, 3)),
-    #         "early_stopping": [True],
-    #         "random_state": [42],
-    #     },
-    # )
-    # sgd_rs_cv.fit(X_train_rob_scaled, y_train)
-    # sgd_prediction = sgd_rs_cv.predict(X_test_rob_scaled)
-    # metrics_df = metrics_df.append(gather_performance_metrics(y_test,
-    #                                                           sgd_prediction,
-    #                                                           "sgd"))
+        # ---- add to a different script ----
+            # model 4 - XGBoost Classifieri
+            # params have to have the same number of values
+    
+        param_bounds={
+            "n_estimators": (0, 1000),
+            'max_depth': (3, 10),
+            "learning_rate": (0.1, 0.8),
+            "gamma": (0, 100),
+            "min_child_weight": (1, 1000),
+            "max_delta_step": (0, 10),
+            "reg_alpha": (0, 3),
+            "reg_lambda": (1, 4),
+            }
+    
+        dtrain = xgb_dmatrix(X_train, label=y_train)
+        def xgb_evaluate(max_depth, gamma, colsample_bytree):
+            params = {"objective": "binary:logistic",
+                      'eval_metric': 'logloss',
+                      "learning_rate": 0.1,
+                      'max_depth': int(max_depth),
+                      'subsample': 0.8,
+                      'gamma': gamma,
+                      'colsample_bytree': colsample_bytree,
+                      "random_state": 42}
+            # Used around 1000 boosting rounds in the full model
+            cv_result = xgb_cv(params=params, dtrain=dtrain, nfold=5)
+    
+            # Bayesian optimization only knows how to maximize, not minimize, so return the negative RMSE
+            return -1.0 * cv_result['test-logloss-mean'].iloc[-1]
+    
+        xgb_bo = BayesianOptimization(xgb_evaluate, {'max_depth': (3, 7),
+                                                     'gamma': (0, 1),
+                                                     'colsample_bytree': (0.3, 0.8)
+                                                     }, random_state=(42))
+        xgb_bo.maximize(init_points=3, n_iter=5, acq='ei', random_state=42)
+    
+        bo_res = pd.DataFrame(xgb_bo.res)
+        max_params = bo_res.loc[bo_res['target']==bo_res['target'].max(), "params"].values[0]
+        max_params["max_depth"] = int(max_params["max_depth"])
+        max_params["objective"] = "binary:logistic"
+        max_params["random_state"]=42
+        max_params_df = pd.DataFrame(max_params, index=['max_params'])
+    
+        # ------------ TESTING ---------------------
+        # Train a new model with the best parameters from the search
+        clf = XGBClassifier(**max_params)
+        clf.fit(X_train, y_train)
+        y_pred = clf.predict(X_test)
+        metrics_df = metrics_df.append(gather_performance_metrics(y_test,
+                                                                  y_pred,
+                                                                  f"xgb_bayes_opt_{scale}"))
+    
+        # second way of optimizing + cv
+        # Classifier
+        bayes_cv_tuner = BayesSearchCV(
+            estimator = XGBClassifier(
+                n_jobs = 1,
+                objective = 'binary:logistic',
+                eval_metric = 'auc',
+                learning_rate= 0.1,
+                subsample=0.8,
+                random_state=42
+            ),
+        search_spaces = {
+                'learning_rate': (0.01, 1.0, 'log-uniform'),
+            'max_depth': (3, 7),
+                'min_child_weight': (1, 10),# no
+                'gamma': (1e-9, 0.5, 'log-uniform'),#no
+                'colsample_bytree': (0.01, 1.0, 'uniform'),#no
+                'colsample_bylevel': (0.01, 1.0, 'uniform'),#no
+                'reg_lambda': (1, 1000, 'log-uniform'),
+                'reg_alpha': (1e-9, 1.0, 'log-uniform'),#no
+                            'n_estimators': (50, 100),
+                #'scale_pos_weight': (1e-6, 500, 'log-uniform')
+    
+    
+                        },    
+            scoring = 'accuracy',
+            cv = StratifiedKFold(
+                n_splits=3,
+                shuffle=True,
+                random_state=42
+            ),
+            n_iter = 10,
+            verbose = 0,
+            refit = True,
+            random_state = 42
+        )
+        def status_print(optim_result):
+            """Status callback durring bayesian hyperparameter search"""
+            
+            # Get all the models tested so far in DataFrame format
+            all_models = pd.DataFrame(bayes_cv_tuner.cv_results_)    
+            
+            # Get current parameters and the best parameters    
+            best_params = pd.Series(bayes_cv_tuner.best_params_)
+            print('Model #{}\nBest Accuracy: {}\nBest params: {}\n'.format(
+                len(all_models),
+                np.round(bayes_cv_tuner.best_score_, 4),
+                bayes_cv_tuner.best_params_
+            ))
+            
+            # Save all model results
+            clf_name = bayes_cv_tuner.estimator.__class__.__name__
+            all_models.to_csv(clf_name+"_cv_results.csv")
+        result = bayes_cv_tuner.fit(X_train, y_train, callback=status_print)
+        y_pred = result.predict(X_test)
+        metrics_df = metrics_df.append(gather_performance_metrics(y_test,
+                                                                  y_pred,
+                                                                  f"xgb_bayes_opt2_{scale}"))
+        metrics_df.to_csv("metrics_results.csv")
 
-    # # model 3 - support vector machines
-    # svm_clf = svm.SVC()
-    # svm_clf.fit(X_train_rob_scaled, y_train)
-    # svm_prediction = svm_clf.predict(X_test_rob_scaled)
 
-    # svm_rs_cv = RandomizedSearchCV(
-    #     estimator=svm_clf,
-    #     param_distributions={
-    #         "C": list(range_inc(0.5, 100, 0.9, 1.2)),
-    #         "break_ties": [False],
-    #         "decision_function_shape": ["ovo", "ovr"],
-    #         "kernel": ["poly", "rbf", "sigmoid"],
-    #         "random_state": [42] 
-    #     }
-    # )
-    # svm_rs_cv.fit(X_train_rob_scaled, y_train)
-    # svm_prediction = svm_rs_cv.predict(X_test_rob_scaled)
-    # metrics_df = metrics_df.append(gather_performance_metrics(y_true=y_test,
-    #                                                           y_pred=svm_prediction,
-    #                                                           model_col="svm"))
-
-    # # metrics_df.to_csv('metrics_df_to_delete.csv')
-    # clf1 = XGBClassifier()
-    # clf1.fit(X_train_rob_scaled, y_train)
-    # xgb_pred = clf1.predict(X_test_rob_scaled)
-    # metrics_df = metrics_df.append(gather_performance_metrics(y_test,
-    #                                                           xgb_pred,
-    #                                                           "xgb"))
-
-    # ---- add to a different script ----
-        # model 4 - XGBoost Classifieri
-        # params have to have the same number of values
-
-    param_bounds={
-        "n_estimators": (0, 1000),
-        'max_depth': (3, 10),
-        "learning_rate": (0.1, 0.8),
-        "gamma": (0, 100),
-        "min_child_weight": (1, 1000),
-        "max_delta_step": (0, 10),
-        "reg_alpha": (0, 3),
-        "reg_lambda": (1, 4),
-        }
-
-    dtrain = xgb_dmatrix(X_train_rob_scaled, label=y_train)
-    def xgb_evaluate(max_depth, gamma, colsample_bytree):
-        params = {"objective": "binary:logistic",
-                  'eval_metric': 'logloss',
-                  "learning_rate": 0.1,
-                  'max_depth': int(max_depth),
-                  'subsample': 0.8,
-                  'gamma': gamma,
-                  'colsample_bytree': colsample_bytree,
-                  "random_state": 42}
-        # Used around 1000 boosting rounds in the full model
-        cv_result = xgb_cv(params=params, dtrain=dtrain, nfold=5)
-
-        # Bayesian optimization only knows how to maximize, not minimize, so return the negative RMSE
-        return -1.0 * cv_result['test-logloss-mean'].iloc[-1]
-
-    xgb_bo = BayesianOptimization(xgb_evaluate, {'max_depth': (3, 7),
-                                                 'gamma': (0, 1),
-                                                 'colsample_bytree': (0.3, 0.8)
-                                                 }, random_state=(42))
-    xgb_bo.maximize(init_points=3, n_iter=5, acq='ei')
-
-    bo_res = pd.DataFrame(xgb_bo.res)
-    max_params = bo_res.loc[bo_res['target']==bo_res['target'].max(), "params"].values[0]
-    max_params["max_depth"] = int(max_params["max_depth"])
-    max_params["objective"] = "binary:logistic"
-    max_params["random_state"]=42
-    max_params_df = pd.DataFrame(max_params, index=['max_params'])
-
-    # ------------ TESTING ---------------------
-    # Train a new model with the best parameters from the search
-    clf = XGBClassifier(**max_params)
-    clf.fit(X_train_rob_scaled, y_train)
-    y_pred = clf.predict(X_test_rob_scaled)
-    metrics_df = metrics_df.append(gather_performance_metrics(y_test,
-                                                              y_pred,
-                                                              "xgb_optimized5"))
-    metrics_df.to_csv("results_to_delete.csv")
 
     # # Report testing and training RMSE
     # print(np.sqrt(mean_squared_error(y_test, y_pred)))
