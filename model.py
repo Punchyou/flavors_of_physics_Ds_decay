2b58d34daac7c0a12e099ca03b0a3e03abaa4034
Maria Pantsiou <maria.th.pantsiou@gmail.com>
2021-05-23 00:17:10 +0100

    Refactor

--- model.py
+++ model.py
@@ -1,26 +1,101 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon May 17 23:38:51 2021
-
-@author: maria
-"""
-
 import matplotlib.pyplot as plt
 import mypy
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from xgboost import XGBClassifier, cv as xgb_cv, DMatrix as xgb_dmatrix
-from bayes_opt import BayesianOptimization
+from xgboost import XGBClassifier
 from sklearn.preprocessing import RobustScaler
-from utils import gather_performance_metrics
+from utils import gather_performance_metrics, plot_learning_curve
 from skopt import BayesSearchCV
 from sklearn.model_selection import StratifiedKFold
+import logging
+from pprint import pformat
+
+
+class BayesSearchCVTuner:
+    """
+    A class that provides an abstractions for the the BayesSearchCV tuner.
+    An XGBClassifier is tuned with Bayes optimization and 3-fold cross
+    validation. The search parameters are:
+        * objective: "binary:logistic"
+        * learning_rate: (0.01, 1.0)
+        * max_depth: (3, 7)
+        * min_child_weight: (1, 10)
+        * gamma: (1e-9, 0.5)
+        * colsample_bytree: (0.01, 1.0)
+        * colsample_bylevel: (0.01, 1.0)
+        * reg_lambda: (1, 1000)
+        * reg_alpha: (1e-9, 1.0)
+        * n_estimators: (50, 100)
+    """
+    def __init__(self, X_train, y_train):
+        self.X_train = X_train
+        self.y_train = y_train
+        self.bayes_cv_tuner = BayesSearchCV(
+            estimator=XGBClassifier(
+                n_jobs=1,
+                objective="binary:logistic",
+                eval_metric="auc",
+                # how many samples will xgboost randomly sample
+                # before growing trees to prevent obverfitting
+                subsample=0.8,
+                use_label_encoder=False,
+                random_state=42,
+            ),
+            search_spaces={
+                "learning_rate": (0.01, 1.0),
+                "max_depth": (3, 7),
+                "min_child_weight": (1, 10),
+                "gamma": (1e-9, 0.5),
+                "colsample_bytree": (0.01, 1.0),
+                "colsample_bylevel": (0.01, 1.0),
+                "reg_lambda": (1, 1000),
+                "reg_alpha": (1e-9, 1.0),
+                "n_estimators": (50, 100),
+            },
+            cv=StratifiedKFold(n_splits=3, shuffle=True, random_state=42),
+            n_iter=10,
+            refit=True,
+            random_state=42,
+        )
+
+    def _status_print(self, optim_result) -> None:
+        """
+        Status callback durring bayesian hyperparameter search.
+
+        Parameters
+        ----------
+        optim_result : scipy.optimize.optimize.OptimizeResult
+
+        Returns
+        -------
+        None.
+        """
+
+        # Get all the models tested so far in DataFrame format
+        all_models = pd.DataFrame(self.bayes_cv_tuner.cv_results_)
+
+        # Get current parameters and the best parameters
+        best_params = pd.Series(self.bayes_cv_tuner.best_params_)
+        logging.info(
+            f"|nModel #{len(all_models)}\nBest AUC: \
+            {np.round(self.bayes_cv_tuner.best_score_, 4)}\nBest params:\n \
+            {pformat(dict(self.bayes_cv_tuner.best_params_), sort_dicts=False)}"
+        )
+
+        # Save all model results
+        clf_name = self.bayes_cv_tuner.estimator.__class__.__name__
+        all_models.to_csv(f"reports/{clf_name}_cv_results.csv", index=False)
+
+    def fit(self):
+        return self.bayes_cv_tuner.fit(
+            self.X_train, self.y_train, callback=self._status_print
+        )
+
 
 def main():
     # load data
-    train_df = pd.read_csv("data/resampled_data.csv", index_col="Unnamed: 0")
+    train_df = pd.read_csv("data/resampled_data.csv")
     y = train_df["signal"]
     X = train_df.drop("signal", axis=1)
 
@@ -29,106 +104,45 @@ def main():
         X, y, test_size=0.2, random_state=42
     )
 
-    # Robust Scaler: doesn't take the median into account
-    # only focuses on the parts where the bulk data is.
+    # scale the data
     rob_scaler = RobustScaler()
     X_train_rob_scaled = rob_scaler.fit_transform(X_train)
     X_test_rob_scaled = rob_scaler.fit_transform(X_test)
 
-    # convert data to DMatrix, the xgboost's internal datastructure
-    dtrain = xgb_dmatrix(X_train_rob_scaled, label=y_train)
+    # tune and fit
+    bayes_tuner = BayesSearchCVTuner(X_train_rob_scaled, y_train)
+    result = bayes_tuner.fit()
 
-    def xgb_evaluate(max_depth, gamma, colsample_bytree):
-        params = {"objective": "binary:logistic",
-                  'eval_metric': 'logloss',
-                  "learning_rate": 0.1,
-                  'max_depth': int(max_depth),
-                  'subsample': 0.8,
-                  'gamma': gamma,
-                  'colsample_bytree': colsample_bytree,
-                  "random_state": 42}
-        # Used around 1000 boosting rounds in the full model
-        cv_result = xgb_cv(params=params, dtrain=dtrain, nfold=5)
-
-        # Bayesian optimization only knows how to maximize, not minimize, so return the negative RMSE
-        return -1.0 * cv_result['test-logloss-mean'].iloc[-1]
-
-    xgb_bo = BayesianOptimization(xgb_evaluate, {'max_depth': (3, 7),
-                                                 'gamma': (0, 1),
-                                                 'colsample_bytree': (0.3, 0.8)
-                                                 }, random_state=(42))
-    xgb_bo.maximize(init_points=3, n_iter=5, acq='ei', random_state=42)
-
-    bo_res = pd.DataFrame(xgb_bo.res)
-    max_params = bo_res.loc[bo_res['target']==bo_res['target'].max(), "params"].values[0]
-    max_params["max_depth"] = int(max_params["max_depth"])
-    max_params["objective"] = "binary:logistic"
-    max_params["random_state"]=42
-    max_params_df = pd.DataFrame(max_params, index=['max_params'])
-
-    # ------------ TESTING ---------------------
-    # Train a new model with the best parameters from the search
-    clf = XGBClassifier(**max_params)
-    clf.fit(X_train_rob_scaled, y_train)
-    y_pred = clf.predict(X_test_rob_scaled)
-    gather_performance_metrics(y_test, y_pred, "xgb_optimized")
-
-    bayes_cv_tuner = BayesSearchCV(
-        estimator = XGBClassifier(
-            n_jobs = 1,
-            objective = 'binary:logistic',
-            eval_metric = 'auc',
-            learning_rate= 0.1,
-            subsample=0.8,
-            random_state=42
-        ),
-    search_spaces = {
-            'learning_rate': (0.01, 1.0, 'log-uniform'),
-            'max_depth': (3, 7),
-            'min_child_weight': (1, 10),
-            'gamma': (1e-9, 0.5, 'log-uniform'),
-            'colsample_bytree': (0.01, 1.0, 'uniform'),
-            'colsample_bylevel': (0.01, 1.0, 'uniform'),
-            'reg_lambda': (1, 1000, 'log-uniform'),
-            'reg_alpha': (1e-9, 1.0, 'log-uniform'),
-            'n_estimators': (50, 100),
-
-
-                    },    
-        scoring = 'accuracy',
-        cv = StratifiedKFold(
-            n_splits=3,
-            shuffle=True,
-            random_state=42
-        ),
-        n_iter = 10,
-        verbose = 0,
-        refit = True,
-        random_state = 42
-    )
-    def status_print(optim_result):
-        """Status callback durring bayesian hyperparameter search"""
-        
-        # Get all the models tested so far in DataFrame format
-        all_models = pd.DataFrame(bayes_cv_tuner.cv_results_)    
-        
-        # Get current parameters and the best parameters    
-        best_params = pd.Series(bayes_cv_tuner.best_params_)
-        print('Model #{}\nBest Accuracy: {}\nBest params: {}\n'.format(
-            len(all_models),
-            np.round(bayes_cv_tuner.best_score_, 4),
-            bayes_cv_tuner.best_params_
-        ))
-        
-        # Save all model results
-        clf_name = bayes_cv_tuner.estimator.__class__.__name__
-        all_models.to_csv(clf_name+"_cv_results.csv")
-    result = bayes_cv_tuner.fit(X_train_rob_scaled, y_train, callback=status_print)
+    # predict
     y_pred = result.predict(X_test_rob_scaled)
-    gather_performance_metrics(y_test,
-                                y_pred,
-                                "xgb_bayes_opt2")
+    metrics = gather_performance_metrics(y_test, y_pred, "xgb_bayes_tuned")
+    logging.info(
+        f"The model predicted signal events with accuracy \
+            {round(metrics['Accuracy'].values[0], 2)}%"
+        )
+
+    # save metrics and prediction
+    metrics.T.to_csv("reports/model_final_metrics.csv")
+    prediction = y_test.to_frame()
+    prediction['y_pred'] = y_pred
+    prediction.to_csv("reports/signal_final_prediction.csv")
 
 
+    # check if the model suffers from bias
+    # plot the learning curve
+    plot_learning_curve(
+        estimator=result.best_estimator_, X=X_train_rob_scaled, y=y_train, n_jobs=8, title="XGBoost Classifier"
+    )
+    plt.show()
+    plt.savefig("plots/learning_curve.png")
+
+    """The first plot is the learning curve
+    The plots in the second row show the times required by the models to train with various sizes of training dataset. The plots in the third row show how much time was required to train the models for each training sizes."""
 if __name__ == "__main__":
+    # set up logging
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s: %(message)s",
+        datefmt="%d-%b%y %H: %M: %S",
+        level=logging.INFO,
+    )
     main()
